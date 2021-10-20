b819a38374948a83a90586a3845fcc7c8c502fce
sapna73 <sg.sapna73@gmail.com>
2021-10-20 19:35:29 +0530

    Resturants pages

---
+++ app/src/main/java/com/example/tamangfoodappservices/ui/HomePage.kt
@@ -0,0 +1,12 @@
+package com.example.tamangfoodappservices.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tamangfoodappservices.R
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
--- app/src/main/java/com/example/tamangfoodappservices/ui/SignIn.kt
+++ app/src/main/java/com/example/tamangfoodappservices/ui/SignIn.kt
@@ -1,12 +1,23 @@
 package com.example.tamangfoodappservices.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.example.tamangfoodappservices.R
 
 class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        var textViewForgot = findViewById<TextView>(R.id.tv_forgot)
+
+        textViewForgot.setOnClickListener(){
+
+            intent = Intent(this, ForgetPassword::class.java)
+            startActivity(intent)
+        }
     }
 }
